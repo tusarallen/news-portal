195ccf1709a6d6e7df5b31f8cd30bc67360be050
email <allentusar909@gmail.com>
2023-03-02 00:06:18 +0600

    almost done news portal

--- scripts/news-portal.js
+++ scripts/news-portal.js
@@ -28,5 +28,47 @@ const fetchCatagoryNews = (category_id, category_name) => {
 
 const showAllNews = (data, category_name) => {
   document.getElementById("news-count").innerText = data.length; 
-  document.getElementById("catagory-name").innerText = category_name; 
+  document.getElementById("catagory-name").innerText = category_name;
+
+  const newsContainer = document.getElementById("all-news");
+  newsContainer.innerText = "";
+  data.forEach(singleNews => {
+    const {_id, image_url, title, details, author, total_view} = singleNews;
+    const card = document.createElement("div");
+    card.classList.add("card", "mb-3");
+    // console.log();
+    card.innerHTML = `
+    <div class="row g-0">
+            <div class="col-md-4">
+              <img src="${image_url}" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">${title}</h5>
+                <p class="card-text">${details.slice(0, 400)}...</p>
+              </div>
+              <div class="card-footer border-0 bg-body d-flex justify-content-between">
+              <div class="d-flex gap-2">
+              <img src="${author.img}" class="img-fluid rounded-circle" height="40" width="40" alt="...">
+              <div>
+              <p class="m-0 p-0">${author.name}</p>
+              <p class="m-0 p-0">${author.published_date}</p>
+              </div>
+              </div>
+              <div class="d-flex align-items-center">
+              <i class="fa-regular fa-eye"></i>
+              <p class="m-0 p-0">${total_view}</p>
+              </div>
+              <div>
+              <i class="fa-regular fa-star"></i>
+              </div>
+              <div>
+              <i class="fa-solid fa-arrow-right"></i>
+              </div>
+              </div>
+            </div>
+          </div>
+    `;
+    newsContainer.appendChild(card);
+  })
 }
